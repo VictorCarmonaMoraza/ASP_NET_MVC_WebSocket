e1c646c671c79f7c6ca1471dc700daf9eeb0626c
Victor <VictorCarmona9035@hotmail.com>
2023-02-18 14:07:13 +0100

    2-Creando Servidor Web Socket

---
+++ MiPrimerServidorWebSocket/Program.cs
@@ -0,0 +1,64 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+
+namespace MiPrimerServidorWebSocket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //IP de mi maquina  // ws:// es el protocolo de un socket
+            string ipServidorWebSocket = DataSocket.PROTOCOLO_SOCKET + DataSocket.IP_SISTEMA;
+
+            //Creamos nuestro servidor de socket
+            WebSocketServer servidorSocket = new WebSocketServer(ipServidorWebSocket);
+
+            //Definimos los clientes qebSocket
+            //Almacena todos los clientes conectados
+            List<IWebSocketConnection> clientesSockets = new List<IWebSocketConnection>();
+
+            //Mensaje para cuando se inicia el socket
+            Console.WriteLine("Servidor web sockets iniciado");
+
+            //Iniciamos el servidor socket
+            servidorSocket.Start(clienteSocket =>
+            {
+                //Evento del cliente, se ejecuta cada vez que nos conectamos al servidor socket
+                clienteSocket.OnOpen = () =>
+                {
+                    //Añado al listado de socket el cliente
+                    clientesSockets.Add(clienteSocket);
+                    Console.WriteLine("Cliente conectado");
+                };
+                //Cunaod tu envias algo al socket
+                clienteSocket.OnMessage = (string texto) =>
+                {
+                    //Emitimos a todos los clientes conectados
+                    clientesSockets.ForEach(p => p.Send(texto));
+
+                    //Es equivalente a :
+                    //for (int i = 0; i < clientesSockets.Count; i++)
+                    //{
+                    //    clientesSockets[i].Send(texto);
+                    //}
+
+                    //Otra forma
+                    //foreach (var cliente in clientesSockets)
+                    //{
+                    //    cliente.Send(texto);
+                    //}
+                };
+                //Cuando ya no estas conectado al socketo sales del socket
+                clienteSocket.OnClose = () =>
+                {
+                    //Eliminamos el cliente que estaba conectadio cuando sale
+                    clientesSockets.Remove(clienteSocket);
+                    Console.WriteLine("Cliente desconectado");
+                };
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MiPrimerServidorWebSocket/const/DataSocket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerServidorWebSocket
+{
+   public class DataSocket
+    {
+        public const string IP_SISTEMA = "192.168.18.6:9001";
+        public const string PROTOCOLO_SOCKET = "ws://";
+    }
+}
